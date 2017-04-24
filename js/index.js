eaae59e20146eb532f9e895f0c6a67d57d5b9047
723 <rb.natsumi@gmail.com>
2017-04-24 15:39:20 +0900

    バグ取り〜 functionを代入してるやーつ

--- js/index.js
+++ js/index.js
@@ -14,20 +14,23 @@ function check_time(in_time,in_min)
   var time = new Date();
   var now_time,now_min;
 
-  if(time.getHours() < 13)
+  if(time.getHours() < 13 && in_time >= 13)
   {
-    now_time = time.getHours() - in_time; 
+    //now_time = time.getHours() - in_time; 
+    console.log("FOO");
+    var timet = time.getHours() + 24;
+    now_time = timet - in_time;
   }
   else
   {
-    now_time = time.getHours;
+    now_time = time.getHours() - in_time;
   }
 
   if(time.getMinutes() < in_min)
   {
     console.log("in");
     var sub_min = in_min - time.getMinutes();
-    now_min = sub_min + (60 - (in_min - sub_min));
+    now_min = 60 - in_min;
     now_time -= 1;
   }
   else
